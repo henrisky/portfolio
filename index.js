93d2f5625022da7d8ad0a0fd515753d4582f4cf9
Henri-webdev <henri.webdev@gmail.com>
2021-03-28 21:51:30 -0500

    fixed file

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 })
 
-app.listen(3000, function() {
+const port = process.env.PORT;
+
+app.listen(port, function() {
   console.log("Running on port 3000.");
 });
